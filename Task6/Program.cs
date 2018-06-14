8e9c6d7e440b35f4e5789222bfcd97c5dfe2428c
Ivan <rovany706@gmail.com>
2018-06-14 18:13:43 +0500

    Add sequence type determination

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -23,12 +23,32 @@ namespace Task6
                 return arr[N - 1];
             return number;
         }
+
+        //0 - монотонно неубывающая (x(n) <= x(n+1))
+        //1 - строго возрастающая   (x(n) < x(n+1))
+        static int SequenceType(double?[] sequence)
+        {
+            bool isEqual = false;
+            for (int i = 1; i < sequence.Length; i++)
+            {
+                if (sequence[i - 1] == sequence[i])
+                {
+                    isEqual = true;
+                    break;
+                }
+            }
+
+            if (isEqual)
+                return 0;
+            return 1;
+        }
+
         static void Main()
         {
             double first = Input.ReadDouble("Введите число a1: ");
             double second = Input.ReadDouble("Введите число a2: ");
             double third = Input.ReadDouble("Введите число a3: ");
-            int N = Input.ReadInt("Введите количество элементво последовательности, которые хотите посмотреть: ");
+            int N = Input.ReadInt("Введите количество элементов последовательности, которые хотите посмотреть: ");
 
             double?[] arr = new double?[N];
             arr[0] = first;
@@ -37,6 +57,8 @@ namespace Task6
             GetNumber(first, second, third, N, ref arr);
             foreach (double i in arr)
                 Console.Write("{0:F4} ", i);
+
+            Console.WriteLine("\nПоследовательность {0}", SequenceType(arr) == 0 ? "монотонно неубывающая." : "строго возрастающая.");
         }
     }
 }
